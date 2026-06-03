3e6c48c6457814c381f23c4bcca5e64605616a6c
Ahsan Ahmed Saif <saifahmed724353@gmail.com>
2026-06-03 22:27:08 +0600

    debugging

--- backend/db/migrations/000004_add_user_id_to_urls.up.sql
+++ backend/db/migrations/000004_add_user_id_to_urls.up.sql
@@ -1,11 +1,14 @@
-ALTER TABLE urls ADD COLUMN IF NOT EXISTS user_id TEXT NOT NULL;
+ALTER TABLE urls 
+ADD COLUMN IF NOT EXISTS user_id TEXT NOT NULL;
 
 DO $$
 BEGIN
     IF NOT EXISTS (
-        SELECT 1 FROM pg_constraint
+        SELECT 1 FROM pg_constraint 
         WHERE conname = 'fk_user_id'
-    ) THEN 
-        ALTER TABLE urls ADD CONSTRAINT IF NOT EXISTS fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+    ) THEN
+        ALTER TABLE urls
+        ADD CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id) REFERENCES users(id);
     END IF;
 END $$;
