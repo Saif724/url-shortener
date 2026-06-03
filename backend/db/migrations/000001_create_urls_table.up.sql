d32d6e78cf4ad9735a6df21f3e40304b221de534
Ahsan Ahmed Saif <saifahmed724353@gmail.com>
2026-06-03 22:19:05 +0600

    debugging

--- backend/db/migrations/000001_create_urls_table.up.sql
+++ backend/db/migrations/000001_create_urls_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
     id TEXT PRIMARY KEY,
     original_url TEXT NOT NULL
 );
--- backend/db/migrations/000003_create_users_table.up.sql
+++ backend/db/migrations/000003_create_users_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id TEXT PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
